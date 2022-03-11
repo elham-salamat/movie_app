2371b1f426c9da355cda6077bdd0191ac5922860
Elham Salamat <35340273+elham-salamat@users.noreply.github.com>
2022-03-11 12:47:01 +0100

    updated authentication for /movies endpoint

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 });
 
 // return a list of all movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
     Movies.find()
     .then((movies) => {
         res.status(200).json(movies)
